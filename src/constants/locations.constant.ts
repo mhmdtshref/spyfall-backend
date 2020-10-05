a93576599649976b278bcd87e197c014436f9d24
Mohamed <mshareef@kiitos-tech.com>
2020-10-05 23:09:56 +0300

    Add new locations to locations list

--- src/constants/locations.constant.ts
+++ src/constants/locations.constant.ts
@@ -5,4 +5,80 @@ export const Locations: Location[] = [
         'ar': 'محطة وقود',
         'en': 'gas station',
     },
+    {
+        'ar': 'محطة الفضاء الدولية',
+        'en': 'international space station',
+    },
+    {
+        'ar': 'الصحراء',
+        'en': 'desert',
+    },
+    {
+        'ar': 'سوبر ماركت',
+        'en': 'super market',
+    },
+    {
+        'ar': 'حديقة حيوانات',
+        'en': 'zoo',
+    },
+    {
+        'ar': 'مكتبة',
+        'en': 'library',
+    },
+    {
+        'ar': 'البيت',
+        'en': 'house',
+    },
+    {
+        'ar': 'جزيرة',
+        'en': 'island',
+    },
+    {
+        'ar': 'الشاطئ',
+        'en': 'beach',
+    },
+    {
+        'ar': 'مسجد',
+        'en': 'mosque',
+    },
+    {
+        'ar': 'كنيسة',
+        'en': 'church',
+    },
+    {
+        'ar': 'سفارة',
+        'en': 'Embassy',
+    },
+    {
+        'ar': 'مستشفى',
+        'en': 'hospital',
+    },
+    {
+        'ar': 'فندق',
+        'en': 'hotel',
+    },
+    {
+        'ar': 'قاعدة عسكرية',
+        'en': 'military base',
+    },
+    {
+        'ar': 'متحف',
+        'en': 'museum',
+    },
+    {
+        'ar': 'استوديو',
+        'en': 'Studio',
+    },
+    {
+        'ar': 'صالون حلاقة',
+        'en': 'barber salon',
+    },
+    {
+        'ar': 'طائرة',
+        'en': 'Plane',
+    },
+    {
+        'ar': 'قطار',
+        'en': 'Train',
+    },
 ];
